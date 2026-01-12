8f7bb29e35532ec32bf4c7d3fbef6db7d74ef51e
Sanket S Suradkar <68096112+SanketSuradkar@users.noreply.github.com>
2026-01-12 15:55:03 +0530

    Enhance comment on module addition issues
    Updated comment to provide more detailed information about issues with adding new modules for icons and languages.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import { useHref } from "react-router-dom";
 import {
-  // when ever trying to add the new module, it stucks and stop working , so do not try to add any new element for the icons and the languages 
+  // when ever trying to add the new module, it stucks and stop working , so do not try to add any new element for the icons and the languages it is having bugs that it does not response at perticular sizes
+  
   mobile,
   backend,
   creator,
